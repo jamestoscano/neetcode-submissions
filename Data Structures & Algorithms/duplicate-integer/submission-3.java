064a763a248854255e5cc2362329ba95cc2d73b0
James Toscano <146594303+jamestoscano@users.noreply.github.com>
2026-09-14 16:12:24 -0400

    Bulk sync: 8 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++){
+            if (map.containsKey(nums[i])){
+                return true;
+            }
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        return false;
+    }
+}
---
+++ Data Structures & Algorithms/dynamicArray/submission-0.java
@@ -0,0 +1,52 @@
+class DynamicArray {
+
+    private int[] array;
+    private int length;
+    private int capacity;
+
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        this.length = 0;
+        this.array = new int[this.capacity];
+    }
+
+    public int get(int i) {
+        return array[i];
+    }
+
+    public void set(int i, int n) {
+        array[i] = n;
+    }
+
+    public void pushback(int n) {
+        if (length == capacity){
+            resize();
+        }
+        array[length] = n;
+        length++;
+    }
+
+    public int popback() {
+        if (length > 0){
+            length--;
+        }
+        return array[length];
+    }
+
+    private void resize() {
+        capacity *= 2;
+        int[] newArr = new int[capacity];
+        for (int i = 0; i < length; i++) {
+            newArr[i] = array[i];
+        }
+        array = newArr;
+    }
+
+    public int getSize() {
+        return this.length;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+}
